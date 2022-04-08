0d9fe9544e4b590f1759bdcb4b3588b5cbfab6dd
yasaemre <emreyasa86@gmail.com>
2022-04-08 08:52:10 -0700

    Initial Commit

---
+++ FetchRewardsIOSChallange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchRewardsIOSChallange
+//
+//  Created by Emre Yasa on 4/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
